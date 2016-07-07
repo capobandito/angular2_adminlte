06021f9f7aea5a38bb3b651655dc233766cccd48
hlavacekm <marek.hlavacek@embedit.cz>
2016-07-07 12:28:41 +0200

    [] base structure, main window, only sidebar menu in angular template (js for this not working)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # angular2_adminlte
 Test to integrate AdminLTE template with angular2
+
+## Installation
+
+npm install
+npm start
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Marek.Hlavacek on 6/16/2016.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
