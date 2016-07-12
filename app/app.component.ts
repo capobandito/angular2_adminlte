1792f57af1ffbfb8891a6f8b833ee8e063ce8b9e
hlavacekm <marek.hlavacek@embedit.cz>
2016-07-12 22:02:28 +0200

    [] refactoring - sidebar menu moved to single component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+
+import {SidebarComponent} from './sidebar.component';
+
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/app.component.html',
+    directives: [SidebarComponent]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+    ngOnInit() {
+
+        //Easy access to options
+        var o = $.AdminLTE.options;
+
+        jQuery.AdminLTE.layout.activate();
+
+        //Enable control sidebar
+        if (o.enableControlSidebar) {
+            $.AdminLTE.controlSidebar.activate();
+        }
+    }
 }
---
+++ app/sidebar.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'sidebar',
+    templateUrl: 'app/sidebar.component.html'
+})
+
+export class SidebarComponent {
+}
