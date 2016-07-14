90c74ddde0365f0979ba4ae143442ed63dd8f62b
hlavacekm <marek.hlavacek@embedit.cz>
2016-07-14 09:33:44 +0200

    [] scheduler component added

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -3,8 +3,10 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { SchedulerComponent } from './scheduler.component';
 import { BatchImportComponent } from './batchimport.component';
 import { ImportComponent } from './import.component';
+import { HomeComponent } from './home.component';
 
 const routes: RouterConfig = [
+    { path: '', component: HomeComponent},
     { path: 'scheduler', component: SchedulerComponent },
     { path: 'batch-import', component: BatchImportComponent },
     { path: 'import', component: ImportComponent }
